6d376748bdee44968123940552fb38bb8881ea15
palshekhar <palshekhar3021@gmail.com>
2024-09-09 01:03:39 +0530

    project pending

---
+++ src/components/details.jsx
@@ -0,0 +1,14 @@
+function Detail() {
+   var date="26/10/2023"
+   var time="10:28:17 AM"
+   
+   return (
+     <div id="detail">
+        <div id="time">{time}</div>
+        <div id="date">{date}</div>
+      </div>
+   )
+
+}
+
+export default Detail;
